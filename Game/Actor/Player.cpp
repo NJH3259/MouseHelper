af5c323c88b0b0740390ca19cd7529e8d389e03b
NamJongha <plazanimo@ajou.ac.kr>
2026-08-26 22:41:01 +0900

    플레이어 마우스 입력 주석 추가

--- Game/Actor/Player.cpp
+++ Game/Actor/Player.cpp
@@ -6,13 +6,22 @@ using namespace Craft;
 
 void Player::Tick(float deltaTime)
 {
+	Actor::Tick(deltaTime);
+
+	//플레이어 액터는 보이지 않지만 위치는 마우스 위치임
+	//position.x = Input::Get().GetMousePosition().x;
+	//position.y = Input::Get().GetMousePosition().y;
+	position = Input::Get().GetMousePosition();
+
 	if (Input::Get().GetKeyDown(VK_LBUTTON))
 	{
 		//현재 마우스 커서 위치에 고양이 액터가 있는지 확인
-
 		//고양이 액터가 있으면
-		//플레이어 액터의 고양이 집은 상태 true
-		//고양이 액터의 Mouse 추적 해제
+		if (CheckCatOnPosition())
+		{
+			//플레이어 액터의 고양이 집은 상태 true
+			//고양이 액터의 Mouse 추적 해제
+		}
 
 
 	}
@@ -22,7 +31,7 @@ void Player::Tick(float deltaTime)
 		int mouseX = Input::Get().GetMousePosition().x;
 		int mouseY = Input::Get().GetMousePosition().y;
 
-		std::string mousePosition = "(" + std::to_string(mouseX) + ", " + std::to_string(mouseY) + ") █ ░ ▒ ▓";
+		std::string mousePosition = "(" + std::to_string(mouseX) + ", " + std::to_string(mouseY) + ")";
 
 		Renderer::GetRenderer().Submit(mousePosition, Vector2(50, 20), Color::White, 5);
 
@@ -31,12 +40,28 @@ void Player::Tick(float deltaTime)
 
 	if (Input::Get().GetKeyUp(VK_LBUTTON))
 	{
-		//고양이 액터를 집은 상태라면
+		if(!isHoldingCat)
+		{
+			return;
+		}
 
+		//고양이 액터를 집은 상태라면
+		
 		//현재 마우스 위치를 집은 고양이 액터 위치로 지정
+		Vector2 position = Input::Get().GetMousePosition();
 
 		//고양이 액터의 쥐 추적 재활성화
 
 		//플레이어 액터의 고양이 집은 상태 false
 	}
+
+	//std::string actorPosition = "(" + std::to_string(position.x) + ", " + std::to_string(position.y) + ")";
+	//Renderer::GetRenderer().Submit(actorPosition, Vector2(20, 20), Color::White, 5);
+}
+
+bool Player::CheckCatOnPosition()
+{
+	//레벨의 액터 리스트에서 현재 마우스 위치와 겹치는 액터가 있는지 검사
+	//있으면 currentCat에 해당 액터 주소 대입 및 return true
+	return false;
 }
--- Game/Actor/Player.h
+++ Game/Actor/Player.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Actor/Actor.h>
+#include <Actor/Cat.h>
 #include <Input/Input.h>
 
 class Player : public Craft::Actor
@@ -14,7 +15,11 @@ public:
 private:
 	virtual void Tick(float deltaTime) override;
 
+	bool CheckCatOnPosition();
+
 private:
 	bool isHoldingCat = false;
+
+	std::shared_ptr<Cat> currentCat = nullptr;
 };
 
